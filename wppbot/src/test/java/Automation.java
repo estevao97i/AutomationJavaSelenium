00e0f15e37f0f1c537a56a28a13b790df2599114
unknown <Lenovo@ntbk-bh3010.bhz.vettatech.com>
2023-06-29 21:53:35 -0300

    add logic to days comingo xaxis and yaxis

--- wppbot/src/test/java/Automation.java
+++ wppbot/src/test/java/Automation.java
@@ -1,4 +1,5 @@
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.asm.Advice;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -12,16 +13,25 @@ import java.awt.*;
 import java.awt.event.InputEvent;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.Scanner;
 
 @Slf4j
 public class Automation {
 
     String XPATHPESQUISARGOOGLE = "//*[@id=\"APjFqb\"]";
     String XPATHBUSCARCONTATOWPP = "//*[@id=\"side\"]/div[1]/div/div/div[2]/div/div[1]/p";
-    String XPATHBUSCARCONTATOMON = "//*[@id=\"pane-side\"]/div[1]/div/div/div[8]/div/div/div/div[2]/div[1]/div[1]/span/span";
+    String XPATHBUSCARCONTATOMON = "//*[@id=\"pane-side\"]/div[1]/div/div/div[6]/div/div/div/div[1]/div/div/img";
     String XPATHCAIXADETEXTO = "//*[@id=\"main\"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p";
     String XPATHCAIXADETEXTOENVIAR = "//*[@id=\"app\"]/div/div/div[3]/div[2]/span/div/span/div/div/div[2]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[1]/p";
+
+    private long coordenateXY(LocalDate date) {
+        var currentDay = LocalDate.now();
+        Duration diff = Duration.between(currentDay.atStartOfDay(), date.atStartOfDay());
+//        System.out.println(diff.toDays());
+        if (diff.toDays() > 6) {
+            coordenateXY(date.plusDays(7));
+        }
+        return diff.toDays();
+    }
 
     @Test
     public void sendMessagesWhatsAppMon() throws InterruptedException, AWTException {
@@ -39,19 +49,30 @@ public class Automation {
         navigator.manage().window().maximize();
 
         navigator.get("https://web.whatsapp.com/");
+//        navigator.get("https://google.com/");
         String parent = navigator.getWindowHandle();
 
         WebDriver newPage = navigator.switchTo().newWindow(WindowType.TAB);
         newPage.get("https://www.google.com/search?q=macacos+tristes&tbm=isch&sa=X&ved=2ahUKEwjs7LrnwOf_AhWVvJUCHfnEDPYQ0pQJegQIDBAB&biw=1680&bih=894&dpr=1");
 
         try {
+
             // These coordinates are screen coordinates
-            int xCoord = 1420;
-            int yCoord = 400;
+            int xCoord = 1470;
+            int yCoord = 420;
+
+            var firstDay = LocalDate.of(2023, 6, 29);
+            var days = coordenateXY(firstDay);
+
+            var newXaxis = ((int) days) * 220 + xCoord;
+
+            if ((days + 1) > 6) {
+                yCoord = 420 + 250;
+            }
 
             // Move the cursor
             Robot robot = new Robot();
-            robot.mouseMove(xCoord, yCoord);
+            robot.mouseMove(newXaxis, yCoord);
 
             //Click on Image
             int mask = InputEvent.BUTTON1_DOWN_MASK;
@@ -72,8 +93,10 @@ public class Automation {
             robot.mouseMove(xCoordShare, yCoordShare);
             Thread.sleep(500);
             robot.mouseMove(2752, 650);
+            Thread.sleep(1000);
             robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
             robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+            Thread.sleep(1000);
 
         } catch (AWTException e) {
         }
@@ -82,8 +105,6 @@ public class Automation {
         navigator.switchTo().window(parent);
 
         Thread.sleep(20000);
-
-        //TODO - FAZER UM CTRL C + CTRL V - CLICANDO E USANDO O ROBOT || FECHAR A JANELA E JOGAR NO MONKEYS - CTRL V E ENTER
 
         navigator.findElement(By.xpath(XPATHBUSCARCONTATOWPP)).sendKeys("monkeys");
         Thread.sleep(2000);
@@ -102,4 +123,6 @@ public class Automation {
                 " dias para o casamento do aquino, Gabri...seu tempo está se esgotando...", Keys.ENTER);
 
     }
+
+
 }
